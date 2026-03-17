fe633a2016c25dbfbc82f16d16c12bd316a52383
MOSHFIQS <moshfiqs.dev@gmail.com>
2026-03-17 22:19:32 +0600

    feat: add unhandled and uncaught exception handler to server.ts

--- src/server.ts
+++ src/server.ts
@@ -16,4 +16,63 @@ const main = async () => {
      }
 };
 
+
+// SIGTERM signal handler
+process.on("SIGTERM", () => {
+    console.log("SIGTERM signal received. Shutting down server...");
+
+    if(server){
+        server.close(() => {
+            console.log("Server closed gracefully.");
+            process.exit(1);
+        });
+    } 
+    
+    process.exit(1);
+    
+})
+
+// SIGINT signal handler
+
+process.on("SIGINT", () => {
+    console.log("SIGINT signal received. Shutting down server...");
+
+    if(server){
+        server.close(() => {
+            console.log("Server closed gracefully.");
+            process.exit(1);
+        });
+
+    }
+
+    process.exit(1);
+});
+
+//uncaught exception handler
+process.on('uncaughtException', (error) => {
+    console.log("Uncaught Exception Detected... Shutting down server", error);
+
+    if(server){
+        server.close(() => {
+            process.exit(1);
+        })
+    }
+
+    process.exit(1);
+})
+
+process.on("unhandledRejection", (error) => {
+    console.log("Unhandled Rejection Detected... Shutting down server", error);
+
+    if(server){
+        server.close(() => {
+            process.exit(1);
+        })
+    }
+
+    process.exit(1);
+})
+
+//unhandled rejection handler
+
 main();
